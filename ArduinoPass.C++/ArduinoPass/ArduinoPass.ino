ef1fb4124910fa091f19994cc73fc0df681c8c2f
mr-rampage <readonly@che.wbac.ca>
2021-02-28 22:09:29 -0500

    :sparkles: Added Keyboard input functionality

--- ArduinoPass.C++/ArduinoPass/ArduinoPass.ino
+++ ArduinoPass.C++/ArduinoPass/ArduinoPass.ino
@@ -2,10 +2,14 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include "Keyboard.h"
 
 #define BUTTON_A  9
 #define BUTTON_B  6
 #define BUTTON_C  5
+
+#define RE(signal, state) (state=(state<<1)|(signal&1)&3)==1
+int btnState[3];
 
 Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &Wire);
 
@@ -21,7 +25,8 @@ const String HANDSHAKE_RESPONSE = "OK";
 void setup() {
   // start serial port at 9600 bps:
   Serial.begin(9600);
-  pinMode(BUTTON_A, INPUT_PULLUP);
+  Keyboard.begin();
+  initializeButtons();
   initializeOled();
   printMessage("Waiting");
 }
@@ -36,9 +41,18 @@ void loop() {
     publishChange(previousMode, mode);
     displayState(mode);
   }
-  if(!digitalRead(BUTTON_A)) {
+  
+  if (RE(digitalRead(BUTTON_A), btnState[0])) {
     mode = WAITING;
     displayState(mode);
+  }
+
+  if (RE(digitalRead(BUTTON_B), btnState[1])) {
+    sendText();
+  }
+
+  if (RE(digitalRead(BUTTON_C), btnState[2])) {
+
   }
 }
 
@@ -96,4 +110,14 @@ void initializeOled() {
   display.display();
   display.setTextSize(1);
   display.setTextColor(SSD1306_WHITE);
+}
+
+void initializeButtons() {
+  pinMode(BUTTON_A, INPUT_PULLUP);
+  pinMode(BUTTON_B, INPUT_PULLUP);
+  pinMode(BUTTON_C, INPUT_PULLUP);
+}
+
+void sendText() {
+  Keyboard.println("Hello!");
 }
