2f2721df61b358df051912ee72dc85a91adadc86
mr-rampage <readonly@che.wbac.ca>
2021-02-28 23:48:25 -0500

    :white_check_mark: Added tests

---
+++ ArduinoPass.FSharp/ArduinoPass.Core.Test/Devices.fs
@@ -0,0 +1,49 @@
+﻿module ArduinoPass.Core.Test.Devices
+
+open ArduinoPass.Core.Device
+
+type ConnectionFailure () =
+    interface IConnectable with
+        member this.connect() =
+            Error "Connection failure"
+            
+        member this.identifier() = "Connection Failure"
+        member this.disconnect() =
+            ()
+
+type InvalidHandshake () =
+    interface IDisconnectable with
+        member this.disconnect() =
+            ()
+    interface IConnectable with
+        member this.connect() =
+            Ok (this :> IQueriableDevice)
+        member this.identifier() = "Invalid Handshake"
+    interface IQueriableDevice with
+        member this.greet(_) =
+           (this :> IUnverfiedDevice)
+
+    interface IUnverfiedDevice with
+        member this.awaitAcknowledgement(_) =
+            Error "Invalid Handshake"
+            
+type ValidHandshake () =
+    interface IDisconnectable with
+        member this.disconnect() =
+            ()
+    interface IConnectable with
+        member this.connect() =
+            Ok (this :> IQueriableDevice)
+
+        member this.identifier() = "Valid Handshake"
+    interface IQueriableDevice with
+        member this.greet(_) =
+           (this :> IUnverfiedDevice)
+
+    interface IUnverfiedDevice with
+        member this.awaitAcknowledgement(_) =
+            Ok (this :> IVerifiedDevice)
+
+    interface IVerifiedDevice with
+        member this.send(_) = failwith "todo"
+        member this.receive() = failwith "todo"
---
+++ ArduinoPass.FSharp/ArduinoPass.Core.Test/DiscoveryProtocolTest.fs
@@ -0,0 +1,25 @@
+﻿module ArduinoPass.Core.Test.DiscoveryProtocolTest
+
+open ArduinoPass.Core.Test.Devices
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open ArduinoPass.Core.DiscoveryProtocol
+open ArduinoPass.Core.Device
+
+[<TestClass>]
+type DiscoveryProtocolTest () =
+    
+    [<TestMethod>]
+    member this.TestConnectionFailure () =
+        (discover (seq { (ConnectionFailure() :> IConnectable) })).IsNone |> Assert.IsTrue
+
+    [<TestMethod>]
+    member this.TestHandshakeFailure () =
+        (discover (seq { (InvalidHandshake() :> IConnectable) })).IsNone |> Assert.IsTrue
+        
+    [<TestMethod>]
+    member this.TestNoDevices () =
+        (discover (Seq.empty)).IsNone |> Assert.IsTrue
+        
+    [<TestMethod>]
+    member this.TestSuccessfulHandshake () =
+        (discover (seq { (ValidHandshake() :> IConnectable) })).IsSome |> Assert.IsTrue
